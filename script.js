7ef2fd10543b37c7c58a5f28ced1a98dcd6d3de1
artem__bakhamatov <ip.bakhmatov@mail.ru>
2025-03-16 22:38:26 +0300

    4. Типы данные в JS

--- script.js
+++ script.js
@@ -1,53 +1,59 @@
-// переменная - это коробочка с данными 
-// Декларирование let x
-// Присваивание x = 1
-// Переменные могут быть в JS var, let, const 
-// var оставлен для обратной совместимости, для чтения для старых браузеров
-// при обновлении js старые фичи не удаляются
+// 8 типов данных 
+// number, bigInt, string, boolean, undefined, null, object, symbol
 
-var x1 = 1;
-let x2 = 1;
-const x3 = 1;
+// Number 
+const age = 28;
+console.log(typeof age); // number
 
-// соглашение об именовании переменных
-// только буквы, цифры, нижнее подчеркивание и знак доллара
-
-console.log(x1, x2, x3); // 1 1 1
-
-// let a;
-// a = 2;
-// console.log(a); 2
-
-// let a = 1, b = 1; c = 1;
-// let a, b, c;
-
-// console.log(a)
-// let a = 1; // тут будет ошибка Uncaught ReferenceError: can't access lexical declaration 'a' before initialization
-// var a = 1; // undefined // имеет глобальную область видимости
-
-// let a = 1;
-// a = 10; // можно перезаписать переменную 
-// console.log(a);
-
-// const b = 2;
-// b = 20; // получим ошибку, так как const нельзя перезаписать 
-// console.log(b);
-
-// {
-//   const x = 100;
-//   console.log(x); // 100
-// }
-// console.log(x); // будет ошибка
-
-// {
-//   var x = 100;
-//   console.log(x); // 100
-// }
-// console.log(x); // 100 // переменную var видно в любом месте
-
-// переменные называть осознано
+// String 
 const firstName = 'Artem';
-const age = 37;
+console.log(typeof firstName); // string
 
-console.log(typeof firstName);  // string
-console.log(typeof age);        // number
+// BigInt
+const bigNumber = 1287644238009876543356n; // в конце больших чисел добавляем букву n
+console.log(typeof bigNumber); // bigint
+
+// Boolean (true или false)
+const isActive = true;
+const isConfirmed = false;
+console.log(typeof isActive); // boolean
+
+// Null - это пусто, либо ничего, другими словами ничего не значит 
+const city = null; // значение присвоено 
+console.log(typeof city); // object //  это не правильно, это значение оставили для совместимости старых браузеров
+// null показывает что это object (просто нужно запомнить)
+
+// Undefined - переменная есть, но она не имеет значения
+let isLoading; // значение не присвоено 
+console.log(isLoading); // undefined // значение 
+console.log(typeof isLoading); // undefined // тип данных 
+// isLoading = undefined;
+isLoading = false;
+console.log(isLoading); // false
+
+// Symbol - используется для уникальных идентификаторов в объекте
+const id = Symbol('123');
+console.log(typeof id); // symbol
+
+// Ссылочные типы
+// Object
+const person = {
+  name: 'Alex', // ключ объекта: значение объекта
+  age: 24,      // ключ объекта: значение объекта
+  id: id,       // ключ объекта: значение объекта
+  isActive: isActive // если ключ и значение совпадают, то можно просто записывать ключ isActive
+};
+console.log(typeof person); // object
+console.log(person); // Object { name: "Alex", age: 24, id: Symbol("123"), isActive: true }
+
+// Array (относится к объекту)
+const numArr = [1, 2, 3, 4, 5];
+console.log(typeof numArr); // object
+console.log(numArr); // Array(5) [ 1, 2, 3, 4, 5 ]
+
+// function (относится к объекту, но тип данных у неё выводится как function, )
+function info() {
+  return 'Hello World!';
+}
+console.log(typeof info); // function (тип данных function нет в js, это объект, нужно запомнить правило)
+
