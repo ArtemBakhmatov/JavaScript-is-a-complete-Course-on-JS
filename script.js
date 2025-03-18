e654599a40592d3bb8de67a33f17986de885ce89
artem__bakhamatov <ip.bakhmatov@mail.ru>
2025-03-18 22:51:37 +0300

    6. Преобразование типов в JS

--- script.js
+++ script.js
@@ -1,73 +1,122 @@
-// Operators
-let lessons = 10;
+let value = 10;
 
-lessons = 10 + 10; // 20
-lessons = 10 - 5; // 5
-lessons = 10 * 10; // 100
-lessons = 10 / 10; // 1
-lessons = 11 % 2; // 1 // остаток от деления 
-// console.log(lessons);
+// число в строку 
 
-// ---------------------------------
-let x;
-x = 2 ** 2; // возведение в степень // 4
-x = 2 ** 3; // возведение в степень // 8
+value = value + '';         // 1 вариант преобразования в строку
+value = `${ value }`;       // 2 вариант преобразования в строку
+value = value.toString();   // 3 вариант преобразования в строку
+value = String(value);      // 4 вариант преобразования в строку
+console.log(value);   // 10 // string
 
-x = 1;
-x = x + 1; // 2 // x++
-x = x - 1; // 1 // x--
+// строку в число
 
-console.log(x);
+value = Number(value);      // 1 вариант преобразования в число
+value = '10';
+value = +value;             // 2 вариант преобразования в число
+value = '10a'
+value = parseInt(value);    // 3 вариант преобразования в число, буквы удаляет, '10a' -> 10
+console.log(value);   // 10 // number
 
-// ---------------------------------
+value = '10.333';
+value = parseFloat(value); // превращает число с плавающей точкой
+console.log(value);   // 10.333 // number
 
-let a = 10;
-let b = a++; // постфиксный инкремент 
-// let b = a--; // постфиксный декремент
+value = parseInt(value); // этот метод делает целое число и удаляет буквы
+console.log(value);   // 10 // number 
 
-console.log(b); // 10
-console.log(a); // 11
+value = 10.12345;
+value = value.toFixed(3); // после плавающей точки остается три числа и становится строкой
+console.log(value);   // 10.123 // string 
 
-let c = 10;
-let d = --c;    // префиксный декремент
-// let d = ++c; // префиксный инкремент 
+value = parseFloat(value); // преобразуем в число с плавающей точкой
+console.log(value);   // 10.123 // number
 
-console.log(d); // 9
-console.log(c); // 9
+// -----------------------------
+// Casting
 
-// ---------------------------------
-// Операторы присваивания
+value = 10 + '10';
+console.log(value);   // 1010 // string
 
-let d1 = 10;
-d1 = d1 + 1; 
-d1 += 1; // сокращенный вариант 
-// d1 -= 1;
-// d1 *= 1;
-// d1 /= 1;
-// d1 %= 1;
-// d1 **= 1;
-console.log(d1); // 12
+value = 10 * '10';
+console.log(value);   // 100 // number
 
-// Операторы сравнения
-// >, <, >=, <=
-// ==, ===, !=, !==
+value = 10 / '10';
+console.log(value);   // 1 // number
 
-console.log(1 == 1); // true // нестрогое равенство 
-console.log(1 === 1); // true // строгое равенство 
+value = 10 - '10';
+console.log(value);   // 0 // number
 
-console.log(1 == '1'); // true // нестрогое равенство // сравнение числа с строкой
-console.log(1 === '1'); // false // строгое равенство // сравнение числа с строкой
-// в проектах всегда использовать === , == -> это уже устаревший вариант
+value = 10 % '10';
+console.log(value);   // 0 // number
 
-console.log(1 != '1'); // false // нестрогое неравенство
-console.log(1 !== '1'); // true // строгое неравенство
-// в проектах всегда использовать !==
+value = 10 + true; // true -> это 1
+console.log(value);   // 11 // number
 
-// ---------------------------------
-// Конкатенация (объединения)
-const greeting = 'Hello ' + 'World!';
+value = 10 + false; // false -> это 0
+console.log(value);   // 10 // number
 
-console.log(greeting); // Hello World!
-console.log(1 + 'hello'); // 1hello // string
-console.log(1 + '1'); // 11 // string
+value = 10 + null; // null -> это ничего 
+console.log(value);   // 10 // number
 
+value = 10 + undefined; // undefined нельзя сложить с другим числом
+console.log(value);   // NaN // не число
+
+value = 10 + NaN; // NaN нельзя сложить с другим числом
+console.log(value);   // NaN // не число
+
+value = 10 + []; // число складываем с пустым массивом будет строка
+console.log(value);   // 10 // string
+
+value = 10 + {}; // число складываем с пустым объектом будет строка
+console.log(value);   // 10[object Object] // string
+
+value = true + true;
+console.log(value);   // 2 // number
+
+value = true + false;
+console.log(value);   // 1 // number
+
+value = false + false;
+console.log(value);   // 1 // number
+
+// -----------------------------
+// == сравнивается с приведением к одному типу
+// === сравнивается по типу и по значению без приведения к одному типу
+
+let res;
+
+res = true == true;
+console.log(res); // true
+
+res = true === true;
+console.log(res); // true
+
+res = true == 1;  // тут сравнением по значению
+console.log(res); // true
+
+res = true === 1; // тут сравнением по значению и по типу данных
+console.log(res); // false 
+
+res = false == 0; // тут сравнением по значению
+console.log(res); // true
+
+res = false === 0; // тут сравнением по значению и по типу данных
+console.log(res); // false 
+
+res = 1 == '1'; 
+console.log(res); // true
+
+res = 1 === '1'; 
+console.log(res); // false
+
+res = null == undefined;
+console.log(res); // true
+
+res = null === undefined;
+console.log(res); // false
+
+res = '' == [];
+console.log(res); // true
+
+res = '' === [];
+console.log(res); // false
