faa2e4d0b174bfa5e9e09f930cfe4a6573c072ed
artem__bakhamatov <ip.bakhmatov@mail.ru>
2025-03-31 17:17:49 +0300

    16. Функции в js, параметры, return

--- script.js
+++ script.js
@@ -1,108 +1,55 @@
-/* 
-  Конструкция switch заменяет собой сразу несколько if.
-  Это более наглядный способ сравнить выражение сразу с несколькими вариантами
-*/
+// functions - base
 
 const log = console.log;
 
-const x = 5 + 5;
-
-switch(x) {
-  case 3:
-    log('Мимо!');
-    break;
-  case 6:
-    log('Ещё нет!');
-    break;
-  case 10:
-    log('Правильно');
-    break;
-  default: 
-    log('Нет ответа');
+// Объявление функции 
+function sayHello() { // функцию объявили // но пока в консоли ничего нет
+  log('Hello my fiends!');
 }
 
-// -----------------------------
+sayHello(); // функцию вызвали // вот теперь есть результат
+sayHello(); // можем вызвать несколько раз
 
-const num = prompt('Введите число от 0 до 7!'); // будет возвращаться строка
+// -------------------------------------------
+// параметры и аргументы функции
+// num1, num2 - это параметры (переменные)
 
-switch(num) {
-  case '0':
-  case '1':
-  case '2':
-    log('Числа: 0 - 2 !');
-    break;
-  case '3':
-  case '4':
-  case '5':
-  case '6':
-  case '7':
-    log('Числа: 3 - 6 !');
-    break;
-  case 7:
-    log('Мы сюда не попадем, так как это число');
-    break;
-  default:
-    log('Нет ответа'); 
+function sum(num1, num2) {
+  log(num1 + num2); 
 }
 
-// -------------------------
-const today = new Date();
-const month = today.getMonth();
+sum(10, 20); // 30 //тут передаем аргументы 10 и 20 ()
+sum(100, 200); // 300 //тут передаем аргументы 100 и 200
 
-switch (month) {
-  case 0:
-    log('Январь');
-    break;
-  case 1:
-    log('Февраль');
-    break;
-  case 2:
-    log('Март');
-    break;
-  case 3:
-    log('Апрель');
-    break;
-  case 4:
-    log('Май');
-    break;
-  case 5:
-    log('Июнь');
-    break;
-  case 6:
-    log('Июль');
-    break;
-  case 7:
-    log('Август');
-    break;
-  case 8:
-    log('Сентябрь');
-    break;
-  case 9:
-    log('Октябрь');
-    break;
-  case 10:
-    log('Ноябрь');
-    break;
-  case 11:
-    log('Декабрь');
-    break;
-  default:
-    log('Без даты'); 
+// --------------------------
+// функция, которая ничего не делает
+
+function doNothing() {}
+// когда вызовем doNothing() то выведет undefined 
+
+// Возвращение числа
+
+function add(num1, num2) {
+  return log(num1 + num2);
+  log('Тут ничего не сработает!')
 }
 
-const hour = today.getHours();
-log(hour);
+add(50, 100);
 
-switch (true) {
-  case hour < 12:
-    log('Доброе утро!'); 
-    break;
-  case hour < 18:
-    log('Добрый день!'); 
-    break;
-  case hour < 22:
-    log('Добрый вечер!'); 
-    break;
-  default:
-    log('Доброй ночи!'); 
-}
+// внутри функции return - это возвращает ответ, и после return дальше не работает
+// а если внутри функции не нет return то будет undefined, 
+
+/*  для примера по поводу undefined
+
+  function sum(num1, num2) {
+    log(num1 + num2); 
+  }
+
+  sum(10, 20); // 30 // сначала ответ 30 и потом выведет undefined в консоли в браузере 
+
+  function sum(num1, num2) {
+    return log(num1 + num2); 
+  }
+
+  sum(10, 20); // 30 // а здесь ответ будет только 30, так как мы добавили в функцию return
+*/
