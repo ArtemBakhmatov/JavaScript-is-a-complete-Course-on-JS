197b58940fead28bb50819198f564ebd23bb93a0
artem__bakhamatov <ip.bakhmatov@mail.ru>
2025-04-01 21:29:09 +0300

    20. Массивы в js, индексы, элементы

--- script.js
+++ script.js
@@ -1,111 +1,73 @@
-// Objects - Объекты 
+// Arrays - Массивы
 
-// Объекты это ссылочный тип переменной 
 /* 
-  У объектов есть свойства.
-  Свойство - это пара "ключ: значение", где ключ - это строка (также называется "именем свойства"),
-  а значение может быть чем угодно.
+  Если необходима упорядоченная коллекция данных, в которой присутствуют 1-й, 2-й, 3-й 
+  элементы и т.д. Например, для хранения списка пользователей, товаров, элементов и т.д..
+
+  В этом случае пользовать объект неудобно, так как он не предоставляет методов управления
+  порядком элементов. Нельзя вставить новое свойство "между" уже существующими.
+  Объекты просто не предназначены для этих целей
+
+  Для хранения упорядоченных коллекций существует особая структура данных, которая 
+  называется массив
 */
 
 const log = console.log;
 
-const hobby = 'hobby';
-const id = '13#smIsd@';
+// const arr = [];
+const arr = new Array();
 
-// const const person = new Object();
-const person = {
-  name: 'Artem',
-  age: 37,
-  isDeveloper: true,
-  address: {
-    country: 'Poland',
-    city: 'Gdansk',
-    street: 'Main street 999'
-  },
-  0: 'just 0',
-  'last name': 'Miracle',
-  greet: function() {
-    log('Hello, I am Artem!');
-  },
-  greetSecond: () => log('greetSecond!'),
-  hobby: 'skiing, coding',
-  // [hobby]: 'skiing, coding', можно писать и таким вариантом
-  id, // тут можем просто ключ прописать, если не будем присваивать другое значение
-  // id: id // результат тот же 
-}
+const numbers = [1, 10, 33, 45, 99, 130];
+const arrayMix = [1, 2, 'hello', true, undefined, null, {}, []];
+// массив внутри массива называется "матрица" или "двумерный массив"
 
-log(person); // Object { name: "Artem", age: 37 }
-log(person.name); // 'Artem'
-log(person.age);  // 33
+log(numbers); // Array(6) [ 1, 10, 33, 45, 99, 130 ]
+log(numbers[0]); // 1
+log(numbers[1]); // 10
 
-log(person['name']);// 'Artem'
-log(person['age']); // 33
-
-log(person.isDeveloper); // true
-log(person.isPositive); // undefined // так как нет этого ключа в объекте
-// log(person.isPositive.qwerty); // error
-
-log(person.address); // Object { country: "Poland", city: "Gdansk", street: "Main street 999" }
-log(person.address.city, person.address.country, person.address.street); // Gdansk Poland Main street 999
-
-// -----------------------
-
-log(person[0], person['0']); // just 0 just 0 // тут уже ключ пишем массивом
-log(person["last name"]); // Miracle // правильно писать так 
-
-delete person[0]; // удаляем ключ 0
-log(person); // Object { name: "Artem", age: 37, isDeveloper: true, address: {…}, "last name": "Miracle" }
-
-// ------------------------------
-
-log(person.greet()); // Hello, I am Artem!
-log(person.greetSecond()); // greetSecond!
-
-const prevName = person.name;
-log(prevName); // Artem
-
-person.name = 'Алёша!';
-log(person.name); // Алёша!
-
-// ----------------------------
-
-log(person.isHappy); // undefined // так как нет этого ключа в объекте
-person.isHappy = true;
-log(person.isHappy); // true
-
-// ---------------------------
-
-log(person.hobby); // skiing, coding
-log(person);
-/* 
-  Object { 
-    name: "Алёша!", 
-    age: 37, isDeveloper: 
-    true, address: {…}, 
-    "last name": "Miracle", 
-    greet: greet(), 
-    greetSecond: greetSecond(), 
-    hobby: "skiing, coding", 
-    id: "13#smIsd@", 
-    isHappy: true 
-  }
-
-*/
-log(person.id); // 13#smIsd@
+log(arrayMix); // Array(8) [ 1, 2, "hello", true, undefined, null, {}, [] ]
+log(typeof numbers); // object
 
 // -----------------------------
+let x = numbers[5];
+log(x); // 130
 
-log('age' in person); // true // 
-log('qwerty' in person); // false // 
+x = numbers[5] + numbers[1];
+log(x); // 140
 
-// -------------------------- 
+// -----------------------------
+numbers[5] = 1000;
+log(numbers[5]); // 1000
 
-const newPerson = person; // присваиваем не объект, а ссылку,
-// поменяли в newPerson , тут же сразу меняется и в person
+log(numbers.length); // 6 // длина массива
 
-log(newPerson === person); // true
+// -----------------------------
+const newArray = new Array(1, 2, 3, 5, 'hello');
+log(newArray); // Array(5) [ 1, 2, 3, 5, "hello" ]
 
-newPerson.age = 20;
+// --------------------------
+// копирование массивов
 
-log(newPerson === person); // true
-log(newPerson.age, person.age); // 20 20
+const copiedArray = numbers;
+log(copiedArray === numbers); // true
+
+copiedArray[0] = 999;
+log(copiedArray === numbers); // true
+log(copiedArray[0], numbers[0]); // 999 999
+
+// ------- методы -----------
+numbers.unshift(111); // добавляет в нулевой массив, а остальные смещаются вперед
+log(numbers); // Array(7) [ 111, 999, 10, 33, 45, 99, 1000 ]
+
+numbers.push(777); // Добавляет в самый конец массива
+log(numbers); // Array(8) [ 111, 999, 10, 33, 45, 99, 1000, 777 ]
+
+numbers.shift(); // удаляет нулевой массив (первый элемент)
+log(numbers); // Array(7) [ 999, 10, 33, 45, 99, 1000, 777 ]
+
+numbers.pop(); // удаляет самый последний элемент в массиве
+log(numbers); // Array(6) [ 999, 10, 33, 45, 99, 1000 ]
+
+const lastValue = numbers.pop();
+log(numbers); // Array(5) [ 999, 10, 33, 45, 99 ]
+log(lastValue); // 1000
