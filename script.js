147a47b75a32221d1708a2eba25dee1dc8571efa
artem__bakhamatov <ip.bakhmatov@mail.ru>
2025-04-06 20:01:13 +0300

    22. Массивы в js, методы, spread, rest, деструктуризация

--- script.js
+++ script.js
@@ -1,246 +1,189 @@
-// Циклы, for, while
+// Array (массивы) - for of, методы, деструктуризация, spread, rest
+
 const styles = `background: lightsalmon`;
 const log = console.log;
-
-// ------- Циклы for ----------
-
+ 
 const cars = ['audi', 'ford', 'mercedes', 'mazda', 'tesla'];
 
-log('1: ', cars[0]);
-log('2: ', cars[1]);
-log('3: ', cars[2]);
-log('4: ', cars[3]);
-log('5: ', cars[4]);
-
-/* 
-  for(стартовое выражение (начало); условное выражение (условие); 
-  инкремент или декремент, повторяется на каждом шаге (итерация)) {
-    логика на каждом шаге (итерации)
-  }
-*/
-// 1 способ
-for(let i = 0; i <= 5; i++) { // когда цикл дойдет до 6 и сразу цикл заканчивается, i - локаль-я пере-я
-  log('--->', i);
-}
-// будет такой ответ
-/* 
-  ---> 0 
-  ---> 1 
-  ---> 2 
-  ---> 3 
-  ---> 4 
-  ---> 5 
-*/
-
-// 2 способ
-let i = 0; // i - глобальная переменная
-for(; i < 5; i++) {
-  log('число: ', i);
-}
-
-// будет такой ответ
-/* 
-  число:  0
-  число:  1
-  число:  2
-  число:  3
-  число:  4 
-*/
-
-// 3 способ
-let y = 0; // i - глобальная переменная
-for(; y < 5;) {
-  log('число: ', y++);
-}
-// ответ будет такой как и во втором способе
-
-// for(;;) {
-//   log('Тут будет вечный цикла и браузер зависнет');
-// }
-
-// -----------------------------------
-// 1 вариант с машинами
-for (let i = 0; i < cars.length; i++) {
-  log(`${ i + 1 }: ${ cars[i] }`);
-}
-
-/* 
-  1: audi 
-  2: ford 
-  3: mercedes 
-  4: mazda 
-  5: tesla
-*/
-
-// 2 вариант с машинами
-for (let i = cars.length - 1; i >= 0; i--) {
-  log(`${ i + 1 }: ${ cars[i] }`);
-}
-
-/* 
-  5: tesla 
-  4: mazda 
-  3: mercedes 
-  2: ford 
-  1: audi
-*/
-
-// --------------------------------------------
-
-for (let i = 0; i < cars.length; i++) {
-  if (cars[i] === 'mercedes') continue; // эту итерацию он пропустит и пойдет дальше
-  log(`${ i + 1 }: ${ cars[i] }`);
-}
-
-/* 
-  1: audi 
-  2: ford 
-  4: mazda 
-  5: tesla 
-*/
-
-for (let i = 0; i < cars.length; i++) {
-  if (cars[i] === 'mercedes') break; // тут цикл останавливается
-  log(`${ i + 1 }: ${ cars[i] }`);
-}
-
-/* 
-  1: audi 
-  2: ford 
-*/
-
-// --------------------------------------------
 const users = [
   { id: 1, name: 'Alex', age: 35, position: 'manager' },
   { id: 2, name: 'Kate', age: 22, position: 'qa' },
-  { id: 3, name: 'Nikita', age: 29, position: 'developer' } 
+  { id: 3, name: 'Nikita', age: 29, position: 'developer' }
 ];
 
-for (let x = 0; x < users.length; x++) {
-  console.group(`Employee ${ x + 1 }`); 
-  log(`Name: ${ users[x].name }`);
-  log(`Age: ${ users[x].age }`);
-  log(`Position: ${ users[x].position }`);
-  console.groupEnd();
+// for of (цикл по массиву)
+
+for (const car of cars) {
+  log(`Car -> ${ car }`);
 }
 
 /* 
-  Employee 1 
-    Name: Alex
-    Age: 35 
-    Position: manager   
-  Employee 2 
-    Name: Kate
-    Age: 22
-    Position: qa 
-  Employee 3
-    Name: Nikita 
-    Age: 29
-    Position: developer 
+  Car -> audi 
+  Car -> ford 
+  Car -> mercedes 
+  Car -> mazda 
+  Car -> tesla 
 */
 
-// ------------------------------
-// 1) Вычислить сумму всех чисел
-
-const numbers1 = [1, 2, 3, 4, 5];
-let sum = 0;
-
-for (let i = 0; i < numbers1.length; i++) {
-  // sum = sum + numbers1[i];
-  sum += numbers1[i];
-}
-
-log(sum); // 15
-
-// 2) Посчитать количество четных чисел в массиве
-
-const numbers2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let eventCount = 0; // Четное число
-for (let i = 0; i < numbers2.length; i++) {
-  if (numbers2[i] % 2 === 0) { // условие на четное число
-    eventCount++
-  }
-}
-log(eventCount); // 5
-
-// -----------------------------------
-// Вложенные циклы
-
-for (let i = 0; i < 3; i++) {
-  log(`%c${ i + 1 } - loop[1]`, styles);
-  for (let j = 0; j < 3; j++) {
-    log(`%c${ j + 1 } - loop[2]`);
-  }
+for (const user of users) {
+  log(`User -> ${ user.name } ${ user.age } ${ user.position }`);
 }
 
 /* 
-  1 - loop[1] 
-    %c1 - loop[2] 
-    %c2 - loop[2] 
-    %c3 - loop[2] 
-  2 - loop[1] 
-    %c1 - loop[2] 
-    %c2 - loop[2] 
-    %c3 - loop[2] 
-  3 - loop[1] 
-    %c1 - loop[2] 
-    %c2 - loop[2] 
-    %c3 - loop[2] 
+  User -> Alex 35 manager 
+  User -> Kate 22 qa 
+  User -> Nikita 29 developer 
 */
 
-// ---------------------------------- 
-// while нужен для бесконечного цикла
-let i1 = 0;
+// ----------- Методы ------------
 
-while (i1 < cars.length) {
-  log(`while - ${ i1 + 1 }`);
-  i1++;
+const numbers1 = [4, 33, 2, 1];
+
+log(numbers1.sort()); // Array(4) [ 1, 2, 33, 4 ] // изменяет массив
+log(numbers1.sort((a, b) => a - b)); // Array(4) [ 1, 2, 4, 33 ] // это правильный вариант
+
+log('reverse', numbers1.reverse()); // reverse Array(4) [ 33, 4, 2, 1 ]
+log(numbers1); // Array(4) [ 33, 4, 2, 1 ] // после мутации перем-я изменилась
+
+const numbers2 = [4, 33, 2, 1];
+
+log(numbers2.toSorted((a, b) => a - b)) // Array(4) [ 1, 2, 4, 33 ]
+log(numbers2); // Array(4) [ 4, 33, 2, 1 ] // тут после мутации перемен-я не изменилась
+
+const newNumbers2 = numbers2.toSorted((a, b) => a - b);
+log(newNumbers2); // Array(4) [ 1, 2, 4, 33 ] // чтобы пер-я изменилась, делаем через новую пер-ю
+
+log(numbers2.toSorted((a, b) => a - b).toReversed()); // Array(4) [ 33, 4, 2, 1 ]
+
+// sort() -> изменяет массив
+// toSorted() -> создает копию нового массива
+
+// ---------------------------------
+
+const newCars = ['nissan', 'volvo'];
+const allCars = cars.concat(newCars); // объединяем два массива в один
+
+log(allCars); // Array(7) [ "audi", "ford", "mercedes", "mazda", "tesla", "nissan", "volvo" ]
+
+log(allCars.toString()); // превращает массив в строку // audi,ford,mercedes,mazda,tesla,nissan,volvo
+log(allCars.join()); // превращает массив в строку // audi,ford,mercedes,mazda,tesla,nissan,volvo
+log(allCars.join('')); // audifordmercedesmazdateslanissanvolvo
+log(allCars.join(' ')); // audi ford mercedes mazda tesla nissan volvo
+log(allCars.join(',')); // audi,ford,mercedes,mazda,tesla,nissan,volvo
+log(allCars.join('-')); // audi-ford-mercedes-mazda-tesla-nissan-volvo
+
+// ----------------------------------
+
+const nestedArrays = [1, 2, [3, 4], [5, 6], [7, [8, 9]], 10];
+
+log(nestedArrays[2]); // Array [ 3, 4 ]
+log(nestedArrays[2][0]); // 3 // здесь получим строку
+
+log(nestedArrays.flat()); // Array(9) [ 1, 2, 3, 4, 5, 6, 7, (2) [8, 9], 10 ];
+// [3, 4] -> первый уровень вложенности
+// [8, 9] -> второй уровень вложенности
+
+log(nestedArrays.flat(2)); // Array(10) [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+log(nestedArrays.flat(Infinity)); // Array(10) [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] // такой же ответ
+// Infinity -> прописываем если мы не знаем сколько уровней вложенности
+
+// ----------------------------------
+// cars = ['audi', 'ford', 'mercedes', 'mazda', 'tesla'];
+log(cars.includes('audi')); // true // есть в массиве cars
+log(cars.includes('lada')); // false
+
+log('hello world'.includes('lo')); // true
+
+log(cars.at(1)); // ford // строка
+log(cars.at(-1)); // tesla // строка
+
+// ---------- static methods ----------
+// статические методы объекта/массива
+
+const num = 123;
+log(Array.isArray(num)); // false
+log(Array.isArray(cars)); // true
+
+const arrayOf = Array.of(1, 2, 'Hello');
+log(arrayOf); // Array(3) [ 1, 2, "Hello" ]
+
+const arrayFrom = Array.from('12345');
+log(arrayFrom); // Array(5) [ "1", "2", "3", "4", "5" ]
+
+// ------------ Деструктуризация -------------
+
+const names = ['Alex', 'Mike', 'Angelina'];
+// const nameAlex = names[0];
+//const nameMike = names[1];
+
+// log(nameAlex, nameMike); // Alex Mike
+
+const [nameAlex, nameMike] = names;
+log(nameAlex, nameMike); // Alex Mike
+
+const name2 = ['Kate'];
+const [nameKate, nameOlga] = name2;
+log(nameKate, nameOlga); // Kate undefined
+
+const [nameKate2, nameOlga2 = 'Olga'] = name2;
+log(nameKate2, nameOlga2); // Kate Olga
+
+// const [counter, setCounter] = useState(0);
+
+// ---------------- spread ------------------
+// оператор расширения 
+// spread -> Раскладывает массив
+
+const allNames = [...names, 'Nik', ...name2];
+log(allNames); // Array(5) [ "Alex", "Mike", "Angelina", "Nik", "Kate" ]
+
+const strHello = 'Hello';
+log([...strHello]); // Array(5) [ "H", "e", "l", "l", "o" ]
+
+const allNumbers = [1, 55, 9, 33, 124, 765, 0, 3, 4];
+
+log(Math.max(...allNumbers)); // 765
+
+// --------------- rest --------------------
+// Остаточные параметры
+// rest -> создает массив
+
+const sum = (...numbers) => {
+  let sum = 0;
+
+  for (const value of numbers) {
+    sum += value;
+  }
+
+  return sum;
 }
+
+log(sum(1, 2, 3)); // 6
+
+const [firstName, ...restNames] = names;
+log(firstName, restNames); // Alex Array [ "Mike", "Angelina" ]
+
+const fn = (arg1, arg2, ...rest) => {
+  // тут всё будет работать
+}
+
+// const fn2 = (arg1, ...rest, ...arg) => {
+//   // тут код сломается так как ...rest должен быть в самом конце
+// }
+
+const getTitleAndDescription = (title, ...description) => {
+  log(`%c${ title }`, styles);
+
+  for (const value of description) {
+    log(`- ${ value }`);
+  }
+}
+
+getTitleAndDescription('JavaScript', 'Язык программирования', 'на нем можно писать всё что угодно', 'гибкий и очень популярный');
 
 /* 
-  while (true) {
-    тут будет бесконечный цикл  
-    break; // тут можем прервать цикл если будет ключевое слово break
-  }
+  JavaScript 
+  - Язык программирования 
+  - на нем можно писать всё что угодно 
+  - гибкий и очень популярный 
 */
-
-while (true) {
-  log(`while(true)`);
-  if ( i1 >= 10) {
-    log(`i1 = ${ i1}`);
-    break;
-  }
-  i1++;
-}
-
-// -------------------------------------
-// do - while
-
-// let i2 = 0;
-
-// do {
-//   log(`do-while - ${ i2 + 1 }`);
-//   //i2++;
-// } while (i2 < cars.length);
-
-// 3) Отсортируйте массив пузырьковой сортировкой
-
-const array = [4, 33, 2, 1];
-
-for (let i = 0; i < array.length; i++) {
-  log(`%c${ i - 1 } - loop[1]`, styles); // если в обратную сторону то ставим знак -
-  for (let y = 0; y < array.length - 1 - i; y++) {
-    log(`%c${ y + 1 } - loop[2] ${ array[y] > array[y + 1] }`);
-    if (array[y] > array[y + 1]) {
-      let temp = array[y];
-      array[y] = array[y + 1];
-      array[y + 1] = temp;
-    }
-  }
-}
-
-log(array); // [1, 2, 4, 33]
-
-// есть другой вариант решения
-log(array.sort((a, b) => a - b)); // по возрастанию // [1, 2, 4, 33]
-log(array.sort((a, b) => a - b).reverse()); // тут по убыванию
