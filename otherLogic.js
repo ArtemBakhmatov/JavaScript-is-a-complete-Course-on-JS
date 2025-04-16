40201afb257883d581201439ed5f777563494fd1
artem__bakhamatov <ip.bakhmatov@mail.ru>
2025-04-16 21:01:24 +0300

    32. События в JS, events, addEventListener

--- otherLogic.js
+++
@@ -1,5 +0,0 @@
-import { getItemToLocalStorage } from './helpers.js';
-
-const numbersArray = getItemToLocalStorage('numbers');
-
-console.log(numbersArray);
--- script.js
+++ script.js
@@ -1,9 +1,61 @@
-import { setItemToLocalStorage } from './helpers.js';
-import greetingFn from './helpers.js';
+// Event - События
 
-console.dir(document.getElementById('heading'));
+const log = console.log;
 
-const numbersArray = [1, 2, 3, 4, 5];
+const btn = document.querySelector('#btn');
+const block = document.querySelector('#block');
+const input = document.querySelector('input');
+const text = document.querySelector('p');
 
-setItemToLocalStorage('numbers', numbersArray);
-greetingFn();
+// btn.onclick = () => log('Кнопка была нажата!');
+
+const btnClickListener = () => {
+  log('Кнопка была нажата!');
+}
+
+btn.addEventListener('click', btnClickListener);
+
+block.addEventListener('click', () => {
+  log('блок была нажат!');
+});
+
+input.addEventListener('input', () => {
+  log('пользователь что-то ввел!');
+});
+
+window.addEventListener('resize', () => {
+  log('изменился размер окна!');
+});
+
+// -------------- removeEventListener ------------------
+
+block.addEventListener('click', () => {
+  btn.removeEventListener('click', btnClickListener);
+});
+
+// --------------- Объект Event --------------
+
+
+input.addEventListener('input', (e) => {
+  log(e);
+  log(e.target);
+  log(e.target.value);
+});
+
+btn.addEventListener('click', (e) => {
+  log(e.clientX, e.clientY);
+  log(e.offsetX, e.offsetY);
+});
+
+window.addEventListener('resize', (e) => {
+  log(e.target.innerWidth, e.target.innerHeight);
+});
+
+text.addEventListener('copy', (e) => {
+  e.preventDefault();
+  log('Копирование запрещено!');
+});
+
+window.addEventListener('beforeunload', (e) => {
+  e.preventDefault();
+});
