6b659532da010c99da0299b1c568d1cb7ec8ea34
artem__bakhamatov <ip.bakhmatov@mail.ru>
2025-04-20 22:09:25 +0300

    37. DICE game app, приложение игра в кости на js

--- script.js
+++ script.js
@@ -1,257 +1,41 @@
-import { 
-  saveTodosIntoLocalStorage, 
-  getTodosFromLocalStorage, 
-  getDateRepresentation 
-} from "./utils.js";
+const container = document.querySelector("[data-container]");
+const gameInfo = document.querySelector("[data-game-info]");
+const actionBtn = document.querySelector("[data-action-btn]");
 
-const addTodoInput = document.querySelector('[data-add-todo-input]');
-const addTodoBtn = document.querySelector('[data-add-todo-btn]');
-const searchTodoInput = document.querySelector("[data-search-todo-input]");
-const todosContainer = document.querySelector("[data-todo-container]");
-const todoTemplate = document.querySelector("[data-todo-template]");
+const generateRandomNumber = () => Math.ceil(Math.random() * 6);
 
-let todoList = getTodosFromLocalStorage();
-let filteredTodosList = [];
-
-addTodoBtn.addEventListener('click', () => {
-  if (addTodoInput.value.trim()) {
-    const newTodo = {
-      id: Date.now(),
-      text: addTodoInput.value.trim(),
-      completed: false,
-      createdAd: getDateRepresentation(new Date())
-    }
-
-    todoList.push(newTodo);
-    addTodoInput.value = '';
-    saveTodosIntoLocalStorage(todoList);
-    renderTodos();
-  }
+actionBtn.addEventListener('click', () => {
+  render();
 });
 
-searchTodoInput.addEventListener('input', (e) => {
-  const searchValue = e.target.value.trim();
+const render = () => {
+  const player1 = generateRandomNumber();
+  const player2 = generateRandomNumber();
 
-  filterAndRenderFilteredTodos(searchValue);
-});
+  container.innerHTML = '';
 
-addTodoInput.addEventListener("input", () => {
-  if (searchTodoInput.value.trim()) {
-    searchTodoInput.value = "";
-    renderTodos();
+  container.insertAdjacentHTML('afterbegin', `
+    <svg class="dice dice-red">
+      <use href="sprites.svg#dice-${ player1 }-icon"></use>
+    </svg>
+
+    <svg class="dice dice-blue">
+      <use href="sprites.svg#dice-${ player2 }-icon"></use>
+    </svg>
+  `);
+
+  if (player1 === player2) {
+    gameInfo.textContent = "НИЧЬЯ!";
+    gameInfo.style.color = "#000";
   }
-})
-
-const filterAndRenderFilteredTodos = (searchValue) => {
-  filteredTodosList = todoList.filter((t) => t.text.includes(searchValue));
-  renderFilteredTodos();
-}
-
-const createTodoLayout = (todo) => {
-  const todoElement = document.importNode(todoTemplate.content, true);
-  // true - это означает вложенные элементы
-  const checkbox = todoElement.querySelector('[data-todo-checkbox]');
-  checkbox.checked = todo.completed;
-
-  const todoText = todoElement.querySelector('[data-todo-text]');
-  todoText.textContent = todo.text;
-
-  const todoCreatedDate = todoElement.querySelector('[data-todo-date]');
-  todoCreatedDate.textContent = todo.createdAt;
-
-  const removeTodoBtn = todoElement.querySelector('[data-remove-todo-btn]');
-  removeTodoBtn.disabled = !todo.completed; // тут означает что кнопка отключена если значение true
-
-  checkbox.addEventListener('change', (e) => {
-    todoList = todoList.map((t) => {
-      if (t.id === todo.id) {
-          t.completed = e.target.checked;
-      }
-      return t;
-    });
-    saveTodosIntoLocalStorage(todoList);
-
-    if (searchTodoInput.value.trim()) {
-      filterAndRenderFilteredTodos(searchTodoInput.value.trim());
-    } else {
-      renderTodos();
-    }
-  });
-
-  removeTodoBtn.addEventListener('click', () => {
-    todoList = todoList.filter((t) => {
-      if (t.id !== todo.id) {
-        return t;
-      }
-    });
-    saveTodosIntoLocalStorage(todoList);
-    
-    if (searchTodoInput.value.trim()) {
-      filterAndRenderFilteredTodos(searchTodoInput.value.trim());
-    } else {
-      renderTodos();
-    }
-  });
-
-  return todoElement;
+  else if (player1 > player2) {
+    gameInfo.textContent = "Победил игрок 1";
+    gameInfo.style.color = "#E98B8B";
+  }
+  else {
+    gameInfo.textContent = "Победил игрок 2";
+    gameInfo.style.color = "#449fcc";
+  }
 };
 
-const renderFilteredTodos = () => {
-  todosContainer.innerHTML = "";
-
-  if (filteredTodosList.length === 0) {
-      todosContainer.innerHTML = `<h3>No todos found...</h3>`;
-      return;
-  }
-
-  filteredTodosList.forEach((todo) => {
-      const todoElement = createTodoLayout(todo);
-      todosContainer.append(todoElement);
-  })
-}
-
-const renderTodos = () => {
-  todosContainer.innerHTML = '';
- 
-  if (todoList.length === 0) {
-    todosContainer.innerHTML = '<h3>No todos...</h3>';
-    return;
-  }
-
-  todoList.forEach((todo) => {
-    const todoElement = createTodoLayout(todo);
-    todosContainer.append(todoElement);
-  })
-};
-
-renderTodos();
-
-/* код автора 
-
-import { getDateRepresentation, saveTodosIntoLocalStorage, getTodosFromLocalStorage } from "./utils.js";
-
-const addTodoInput = document.querySelector("[data-add-todo-input]");
-const addTodoBtn = document.querySelector("[data-add-todo-btn]");
-const searchTodoInput = document.querySelector("[data-search-todo-input]");
-const todosContainer = document.querySelector("[data-todo-container]");
-const todoTemplate = document.querySelector("[data-todo-template]");
-
-let todoList = getTodosFromLocalStorage();
-let filteredTodoList = [];
-
-addTodoBtn.addEventListener("click", () => {
-    if (addTodoInput.value.trim()) {
-        const newTodo = {
-            id: Date.now(),
-            text: addTodoInput.value.trim(),
-            completed: false,
-            createdAt: getDateRepresentation(new Date()),
-        }
-        todoList.push(newTodo);
-        addTodoInput.value = "";
-
-        saveTodosIntoLocalStorage(todoList);
-        renderTodos();
-    }
-})
-
-addTodoInput.addEventListener("input", () => {
-    if (searchTodoInput.value.trim()) {
-        searchTodoInput.value = "";
-        renderTodos();
-    }
-})
-
-searchTodoInput.addEventListener("input", (e) => {
-    const searchValue = e.target.value.trim();
-
-    filterAndRenderFilteredTodos(searchValue);
-})
-
-const filterAndRenderFilteredTodos = (searchValue) => {
-    filteredTodoList = todoList.filter((t) => t.text.includes(searchValue));
-    renderFilteredTodos();
-}
-
-const createTodoLayout = (todo) => {
-    const todoElement = document.importNode(todoTemplate.content, true);
-
-    const checkbox = todoElement.querySelector("[data-todo-checkbox]");
-    checkbox.checked = todo.completed;
-
-    const todoText = todoElement.querySelector("[data-todo-text]");
-    todoText.textContent = todo.text;
-
-    const todoCreatedDate = todoElement.querySelector("[data-todo-date]");
-    todoCreatedDate.textContent = todo.createdAt;
-
-    const removeTodoBtn = todoElement.querySelector("[remove-todo-btn]");
-    removeTodoBtn.disabled = !todo.completed;
-
-    checkbox.addEventListener("change", (e) => {
-        todoList = todoList.map((t) => {
-            if (t.id === todo.id) {
-                t.completed = e.target.checked;
-            }
-            return t;
-        })
-
-        saveTodosIntoLocalStorage(todoList);
-
-        if (searchTodoInput.value.trim()) {
-            filterAndRenderFilteredTodos(searchTodoInput.value.trim());
-        } else {
-            renderTodos();
-        }
-    })
-
-    removeTodoBtn.addEventListener("click", () => {
-        todoList = todoList.filter((t) => {
-            if (t.id !== todo.id) {
-                return t;
-            }
-        })
-
-        saveTodosIntoLocalStorage(todoList);
-
-        if (searchTodoInput.value.trim()) {
-            filterAndRenderFilteredTodos(searchTodoInput.value.trim());
-        } else {
-            renderTodos();
-        }
-    });
-
-    return todoElement;
-}
-
-const renderTodos = () => {
-    todosContainer.innerHTML = "";
-
-    if (todoList.length === 0) {
-        todosContainer.innerHTML = `<h3>No todos...</h3>`;
-        return;
-    }
-
-    todoList.forEach((todo) => {
-        const todoElement = createTodoLayout(todo);
-        todosContainer.append(todoElement);
-    })
-}
-
-const renderFilteredTodos = () => {
-    todosContainer.innerHTML = "";
-
-    if (filteredTodoList.length === 0) {
-        todosContainer.innerHTML = `<h3>No todos found...</h3>`;
-        return;
-    }
-
-    filteredTodoList.forEach((todo) => {
-        const todoElement = createTodoLayout(todo);
-        todosContainer.append(todoElement);
-    })
-}
-
-renderTodos();
-
-*/
+render();
